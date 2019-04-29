8380fc3e9c7055a084f1f0a06a6bc8d7abfda461
Kevin <45358947+krayt0n3@users.noreply.github.com>
2019-04-29 07:33:26 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-
-
-
 var query;
 $(document).on('click', '#submit', function(event) {
 event.preventDefault();
@@ -22,14 +19,14 @@ var queryURL = "https://newsapi.org/v2/everything?q=" + query + "&from=" + weekT
         for (let i = 1; i < 5; i++) {
             $('#news').append(`
 
+
 <div class="card" style="width: 18rem;">
-  <img src="${response.articles[i].urlToImage}" class="card-img-top" alt="${response.articles[i].publishedAt}">
-  <div class="card-body">
-    <h5 class="card-title">${response.articles[i].title}</h5>
-    <p class="card-text">${response.articles[i].description}</p>
-    <a href="${response.articles[i].url}" class="btn btn-primary">Learn More</a>
-  </div>
+  <ul class="list-group list-group-flush">
+  <a href="${response.articles[i].url}" target="_blank"><li class="list-group-item">${response.articles[i].title}</li></a>
+  </ul>
 </div>
+
+
 
     `);  $('#newsinput').val('');
     }
